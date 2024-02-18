c9e654f883481fd990d7a190f0cb1d9dd41a1ed4
Kunal Prajapati <kunalprajapati544@gmail.com>
2024-02-18 23:00:19 +0530

    thirdStage completed

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import database
 menu = """Please select one of the following options:
 1) Add new movie.
 2) View upcoming movies.
-3) View all movies
-4) Watch a movie
+3) View all movies.
+4) Watch a movie.
 5) View watched movies.
-6) Delete movie.
-7) Exit.
+6) Add New User.
+7) Search movie.
+8) Exit.
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -19,54 +20,94 @@ database.create_tables()
 
 
 def add_movie():
+    """Add a new movie to the database."""
     title = input("Enter the movie title: ")
     release_date = input("Enter the release date (dd-mm-YYYY): ")
+
     parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
     release_timestamp = parsed_date.timestamp()
-    database.add_movie(title, release_timestamp)
+
+    response = database.add_movie(title, release_timestamp)
+    print(response)
+
+
+def add_user():
+    """Add a new user to the database."""
+    username = input("Enter the username: ")
+    response = database.add_user(username)
+    print(response)
 
 
 def print_movies_list(heading, movies_list):
-    print(f"-----{heading} Movies!-----\n")
-    for row in movies_list:
-        dateTime = datetime.datetime.fromtimestamp(row[1])
+    """Print a list of movies with a specific heading."""
+    print(f"\n-----{heading} Movies!-----\n")
+    for _id, title, release_date in movies_list:
+        dateTime = datetime.datetime.fromtimestamp(release_date)
         date = dateTime.strftime("%b %d %Y")
-        print(f"Title: {row[0]} (Released on {date})\n")
+        print(f"{title} (Released on {date}, and the id is {_id})\n")
         print("---------\n")
 
 
-def print_watched_movies(username,movies_list):
-    print(f"----{username}'s watched movies-----\n")
-    for row in movies_list:
-        print(f"Title: {row[1]}\n")
-    print(f'---------\n')
-
 def watch_movie():
+    """Watch a movie by updating the watched table."""
     username = input("Username: ")
-    title = input("Enter the title of watched movie: ")
-    database.watch_movie(username, title)
+    movie_id = input("Enter the id of watched movie: ")
+    database.watch_movie(username, movie_id)
 
 
-def delete_movie():
-    title = input("Enter the title of the movie: ")
-    database.delete_movie(title)
+def search_movies():
+    """Search for movies based on the title."""
+    pattern = input("Enter the title of the movie: ")
+    movies_list = database.search_movies(pattern)
+    if movies_list:
+        print_movies_list("Found", movies_list)
+    else:
+        print("Found no movies :(")
 
-while (user_input := input(menu)) != "7":
+
+while (user_input := input(menu)) != "8":
+
     if user_input == "1":
         add_movie()
+
     elif user_input == "2":
-        movies_list = database.get_movies(True)
-        print_movies_list("Upcoming", movies_list)
+        movies_list, response = database.get_movies(True)
+
+        if response != "OK":
+            print(response)
+        elif movies_list:
+            print_movies_list("Upcoming", movies_list)
+        else:
+            print("\nNo upcoming movies :(\n")
+
     elif user_input == "3":
-        movies_list = database.get_movies()
-        print_movies_list("All", movies_list)
+        movies_list, response = database.get_movies()
+        if response != "OK":
+            print(response)
+        elif movies_list:
+            print_movies_list("All", movies_list)
+        else:
+            print("No Movies available :(")
+
     elif user_input == "4":
         watch_movie()
+
     elif user_input == "5":
         username = input("Username: ")
-        movies_list = database.get_watched_movies(username)
-        print_watched_movies(username, movies_list)
+
+        movies_list, response = database.get_watched_movies(username)
+        if response != "OK":
+            print(response)
+        elif movies_list:
+            print_movies_list(f"{username}'s Watched", movies_list)
+        else:
+            print(f"\n{username} has watched no movies yet!")
+
     elif user_input == "6":
-        delete_movie()
+        add_user()
+
+    elif user_input == "7":
+        search_movies()
+
     else:
         print("Invalid input, please try again!")
--- database.py
+++ database.py
@@ -2,83 +2,118 @@ import datetime
 import sqlite3
 
 connection = sqlite3.connect("data.db")
+connection.execute(
+    "PRAGMA foreign_keys = ON;"
+)  # only after this foreign_keys support starts
 
 create_movie_table = """ CREATE TABLE IF NOT EXISTS movies(
-                            title TEXT,
-                            release_timestamp REAL);"""
-
-create_watched_table = """CREATE TABLE IF NOT EXISTS watched(
-                            user TEXT,
-                            title TEXT);"""
-
-insert_movies = """ INSERT INTO movies 
-                    (title,release_timestamp) 
-                    VALUES (?,?);"""
-
+id INTEGER PRIMARY KEY,
+title TEXT COLLATE NOCASE,
+release_timestamp REAL,
+UNIQUE (title,release_timestamp) );"""
+create_user_table = (
+    """CREATE TABLE IF NOT EXISTS users(username TEXT COLLATE NOCASE PRIMARY KEY);"""
+)
+create_watched_table = """CREATE TABLE IF NOT EXISTS watched (
+username TEXT COLLATE NOCASE,
+movie_id INTEGER,
+PRIMARY KEY (username, movie_id),
+FOREIGN KEY (username) REFERENCES users(username),
+FOREIGN KEY (movie_id) REFERENCES movies(id)
+);"""
+select_watched_movies = """SELECT movies.* 
+FROM movies 
+JOIN watched ON movies.id = watched.movie_id
+WHERE watched.username = ?;"""
+insert_movies = """INSERT INTO movies (title,release_timestamp) VALUES (?,?);"""
+insert_user = """INSERT INTO users (username) VALUES (?);"""
+insert_watched_movies = """INSERT INTO watched(username,movie_id) VALUES (?,?);"""
 select_all_movies = """SELECT * FROM movies;"""
-
-
-select_upcoming_movies = """SELECT * 
-                            FROM movies 
-                            WHERE release_timestamp > ?;"""
-
-select_watched_movies =  """SELECT * 
-                            FROM watched 
-                            WHERE user = ?;"""
-
-insert_watched_movies = """INSERT INTO watched
-                            (user,title)
-                            VALUES (?,?);"""
-
-update_watch_movie = """UPDATE movies
-                        SET watched = 1
-                        WHERE title = ?;"""
-
-delete_movie_from_movies =  """DELETE FROM movies
-                                WHERE title = ?;"""
-
-delete_movie_from_watched = """DELETE FROM watched
-                                WHERE title = ?;"""
+select_upcoming_movies = """SELECT * FROM movies WHERE release_timestamp > ?;"""
+update_watch_movie = """UPDATE moviesSET watched = 1 WHERE title = ?;"""
+search_movies_query = """SELECT * FROM movies WHERE title LIKE ?;"""
+check_user = "SELECT 1 FROM users WHERE username = ?;"
 
 
 def create_tables():
     with connection:
+        connection.execute(create_user_table)
         connection.execute(create_movie_table)
         connection.execute(create_watched_table)
 
 
 def add_movie(title, release_timestamp):
-    with connection:
-        connection.execute(insert_movies, (title, release_timestamp))
+    try:
+        with connection:
+            connection.execute(insert_movies, (title, release_timestamp))
+        return "Movie added Successfully!"
+    except Exception as e:
+        return f"Error: {e}.\n"
+
+
+def add_user(username):
+    try:
+        with connection:
+            connection.execute(insert_user, (username,))
+        return "User Created Successfully!"
+    except Exception as e:
+        return f"Error: {e}\n"
 
 
 def get_movies(upcoming=False):
-    with connection:
-        cursor = connection.cursor()
-        if upcoming:
-            today_timestamp = datetime.datetime.today().timestamp()
-            # (today_timestamp,) this comma is neccssary because the parameters take a tuple
-            cursor.execute(select_upcoming_movies, (today_timestamp,))
-        else:
-            cursor.execute(select_all_movies)
+    cursor = connection.cursor()
+    try:
+        with connection:
+            if upcoming:
+                today_timestamp = datetime.datetime.today().timestamp()
+                # (today_timestamp,) this comma is neccssary because the parameters take a tuple
+                cursor.execute(select_upcoming_movies, (today_timestamp,))
+            else:
+                cursor.execute(select_all_movies)
 
-    return cursor.fetchall()
+        return cursor.fetchall(), "OK"
+    except Exception as e:
+        return [], f"Error: {e}\n"
+
+
+def check_user_exists(username):
+    cursor = connection.cursor()
+    with connection:
+        cursor.execute(check_user, (username,))
+    result = cursor.fetchone()
+
+    if result is not None:
+        return True
+
+    return False
 
 
 def get_watched_movies(username):
-    with connection:
+
+    if check_user_exists(username) == True:
         cursor = connection.cursor()
-        cursor.execute(select_watched_movies,(username,))
+        try:
+            with connection:
+                cursor.execute(select_watched_movies, (username,))
+
+            return cursor.fetchall(), "OK"
+        except Exception as e:
+            return [], f"Error: {e}\n"
+    else:
+        return [], f"Error: {username} does not exists!\n"
+
+
+def watch_movie(username, movie_id):
+    try:
+        with connection:
+            connection.execute(insert_watched_movies, (username, movie_id))
+    except Exception as e:
+        print(f"Error: {e}\n")
+
+
+def search_movies(pattern):
+    cursor = connection.cursor()
+    with connection:
+        cursor.execute(search_movies_query, (f"%{pattern}%",))
 
     return cursor.fetchall()
-
-
-def watch_movie(username,title):
-    with connection:
-        connection.execute(insert_watched_movies,(username,title))
-
-
-def delete_movie(title):
-    with connection:
-        connection.execute(delete_movie_from_movies,(title,))
-        connection.execute(delete_movie_from_watched,(title,))
